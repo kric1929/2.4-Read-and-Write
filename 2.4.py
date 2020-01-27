94655a17e5fed446652e29829a3d8965d5810f56
Mihail Zharkov <kric19299@gmail.com>
2020-01-27 12:26:35 +0300

    fixed

--- 2.4.py
+++ 2.4.py
@@ -1,23 +1,28 @@
-with open('recipes.txt', encoding='utf-8') as f:
-    cook_book = {}
-    while True:
-        dish = f.readline().strip()
-        cook_book[dish] = []
-        person = f.readline().strip()
-        i = 0
-        while i < int(person):
-            ingredients = f.readline().strip().split(' | ')
-            cook_book[dish].append({'ingredient_name': ingredients[0], 'quantity': int(ingredients[1]),
-                                    'measure': ingredients[2]})
-            i += 1
-        if f.readline() == '\n':
-            continue
-        else:
-            break
-# print(cook_book)
+from pprint import pprint
+
+
+def cook_book_dict():
+    with open('recipes.txt', encoding='utf-8') as f:
+        cook_book = {}
+        while True:
+            dish = f.readline().strip()
+            cook_book[dish] = []
+            person = f.readline().strip()
+            i = 0
+            while i < int(person):
+                ingredients = f.readline().strip().split(' | ')
+                cook_book[dish].append({'ingredient_name': ingredients[0], 'quantity': int(ingredients[1]),
+                                        'measure': ingredients[2]})
+                i += 1
+            if f.readline() == '\n':
+                continue
+            else:
+                break
+    return cook_book
 
 
 def get_shop_list_by_dishes(dishes, person_count):
+    cook_book = cook_book_dict()
     ingredients_dict = {}
     dish_ingredients = []
     if type(dishes) == list:
@@ -32,10 +37,14 @@ def get_shop_list_by_dishes(dishes, person_count):
     for ingredient in dish_ingredients:
         for ingr in ingredient:
             ingr_name = ingr['ingredient_name']
-            ingredients_dict[ingr_name] = ingr
             del ingr['ingredient_name']
-            ingr['quantity'] = ingr['quantity'] * person_count
-    print(ingredients_dict)
+            if ingr_name not in ingredients_dict:
+                ingredients_dict[ingr_name] = ingr
+                ingredients_dict[ingr_name]['quantity'] = ingredients_dict[ingr_name][
+                                                              'quantity'] * person_count
+            elif ingr_name in ingredients_dict:
+                ingredients_dict[ingr_name]['quantity'] += (ingr['quantity'] * person_count)
 
+    pprint(ingredients_dict)
 
-get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 3)
+get_shop_list_by_dishes(['Фахитос', 'Омлет'], 3)
