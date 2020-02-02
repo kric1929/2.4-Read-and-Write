fbc8b1157f1bdbd21c07092962746f89e525960a
Mihail Zharkov <kric19299@gmail.com>
2020-02-02 11:48:02 +0300

    fixed a bug with identical dishes

--- 2.4.py
+++ 2.4.py
@@ -24,27 +24,17 @@ def cook_book_dict():
 def get_shop_list_by_dishes(dishes, person_count):
     cook_book = cook_book_dict()
     ingredients_dict = {}
-    dish_ingredients = []
-    if type(dishes) == list:
-        for dish in dishes:
-            for key, value in cook_book.items():
-                if dish == key:
-                    dish_ingredients.append(value)
-    else:
-        for key, value in cook_book.items():
-            if dishes == key:
-                dish_ingredients.append(value)
-    for ingredient in dish_ingredients:
-        for ingr in ingredient:
-            ingr_name = ingr['ingredient_name']
-            del ingr['ingredient_name']
-            if ingr_name not in ingredients_dict:
-                ingredients_dict[ingr_name] = ingr
-                ingredients_dict[ingr_name]['quantity'] = ingredients_dict[ingr_name][
-                                                              'quantity'] * person_count
-            elif ingr_name in ingredients_dict:
-                ingredients_dict[ingr_name]['quantity'] += (ingr['quantity'] * person_count)
-
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredients in cook_book[dish]:
+                if ingredients['ingredient_name'] not in ingredients_dict:
+                    ingredients_dict[ingredients['ingredient_name']] = ingredients
+                else:
+                    ingredients_dict[ingredients['ingredient_name']]['quantity'] += ingredients['quantity']
+    for key, value in ingredients_dict.items():
+        del value['ingredient_name']
+        value['quantity'] *= person_count
     pprint(ingredients_dict)
 
-get_shop_list_by_dishes(['Фахитос', 'Омлет'], 3)
+
+get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2)
